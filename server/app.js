aed718253dcad921ee47734b4d4cd4ed0939f090
Jazmin Tweedle <112340988+jtweedle1@users.noreply.github.com>
2025-05-07 12:32:15 +0000

    Clean up app.js

--- server/app.js
+++ server/app.js
@@ -4,17 +4,20 @@ const dotenv = require('dotenv');
 
 dotenv.config(); // Loads the env file
 
-const summarizeRoutes = require('./routes/summarize');
-const diagramRoutes = require('./routes/diagram');
-
 const app = express(); // Create an express app instance
+
+// Middleware
 app.use(cors()); // Enable CORS globally 
 app.use(express.json()); // Middleware to parse incoming JSON in request bodies
 
+// Routes
+const summarizeRoutes = require('./routes/summarize');
+const diagramRoutes = require('./routes/diagram');
 app.use('/api/summarize', summarizeRoutes);
 app.use('/api/diagram', diagramRoutes);
 
 app.get('/', (req, res) => res.send('API working!'));
 
+// Start server
 const PORT = process.env.PORT || 3000; // Use port defined in env or default to 3000
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`)); // Starts server and listens on specified port
